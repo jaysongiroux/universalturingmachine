a014dccafa25e8ac37971c07f332aa12c9be9557
jaysongiroux <46802437+jaysongiroux@users.noreply.github.com>
2019-10-14 22:22:05 -0400

    fixed stability and added function for checking syntax of input

--- GUI/linkers/input.js
+++ GUI/linkers/input.js
@@ -1,18 +1,24 @@
 let {PythonShell} = require('python-shell');
 var path = require("path");
 
-function input(){
-  var input = document.getElementById("INPUT").value;
+function checkSyntax(input){
+  //  checks the input to determine if the input is valid
 
-  var options = {
+}
+
+function input(){
+  let input = document.getElementById("INPUT").value;
+
+  let options = {
     scriptPath : path.join(__dirname, '/../engine/'),
     args : [input]
   };
 
   let pyshell = new PythonShell('interface.py', options);
 
-
   pyshell.on('message', function(message) {
     alert(message);
+    let output = document.getElementById('OUTPUT');
+    output.innerHTML = message;
   });
 }
