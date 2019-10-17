9dcd8aeac2e813050c7eb417c81822d8da960853
jaysongiroux <46802437+jaysongiroux@users.noreply.github.com>
2019-10-17 00:43:05 -0400

    unit tests started and position for 'q' inputs implemented

---
+++ Unit_tests/unit test 1.py
@@ -0,0 +1,24 @@
+from engine.turingMachine import position as pos
+from engine.turingMachine import parseInput as parse
+from colorama import Fore
+
+class assertion:
+    def equals(expected, intial, message):
+        if expected == intial:
+            print(Fore.GREEN + "[PASS] " + message)
+        else:
+
+            print(Fore.RED + "[FAIL] " + message + ". received input: ", intial)
+            return
+
+
+def positionTest():
+    assertion.equals(1, pos("q1"),"input: 'q1' should equal '1'")
+    assertion.equals(10, pos("q10"), "input: 'q1' should equal '1'")
+    assertion.equals(None, pos("1"),"input: '1' should equal 'None'")
+
+def parseTest():
+    assertion.equals(['{', 'q1', 'a', 'q3', 'c', 'R', '}'],parse("{q1,a,q3,c,R}"),"input: '{q1,a,q3,c,R}' should equal '['{', 'q1', 'a', 'q3', 'c', 'R', '}']'")
+
+positionTest()
+parseTest()
--- engine/turingMachine.py
+++ engine/turingMachine.py
@@ -1,14 +1,18 @@
-    #####################
-    ##  INPUT PARSING  ##
-    #####################
+import re
+#####################
+##  INPUT PARSING  ##
+#####################
+
 # example:
 # Input is "{q1,a,q3,c,R}"
-# Output is "['{', 'q1', 'q3', 'R', '}']"
+# Output is "['{', 'q1', 'a','q3','c', 'R', '}']"
+# todo: if input does not fit requiered input. return null
 def parseInput(input):
     logger = []
     evaled = []
     tempString = "[INPUT] ", input
     logger.append(tempString)
+
     for i in range(len(input)):
         if input[i] == "{" or input[i] == "}":
             evaled.append(input[i])
@@ -18,6 +22,24 @@ def parseInput(input):
                 evaled.append(input[i])
             else:
                 evaled.append(input[i] + input[i+1])
+        elif input[i] == "a" or input[i] == "v" or input[i] == "c" or input[i] == "R" or input[i] == "L" or input[i] == "Δ":
+            evaled.append(input[i])
     tempString = "[Parsed Input]: ", evaled
     logger.append(tempString)
     return evaled
+
+# determine the position from the Q value
+# input "q1" output "1" for position 1 in the array
+# can be changed the offset if needed
+def position(a):
+
+    offset = 0
+    reg  = "[0-9]*"
+    # if the unit contains "q"
+    if "q" in a:
+        x = re.findall(reg, a, flags=0)
+        return int(x[1]) + offset
+    else:
+        return None
+
+
