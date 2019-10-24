8c55c1eb5de1340a9068ec3bdd90a1e494feafef
jaysongiroux <46802437+jaysongiroux@users.noreply.github.com>
2019-10-24 10:23:42 -0400

    adding tape field, parsing tape and returning array

--- engine/interface.py
+++ engine/interface.py
@@ -4,11 +4,12 @@ import turingMachine as tm
 import re
 
 option = sys.argv[1]
+tape = sys.argv[2]
 
 # this is the script the javascript executes importing another python file for turing machines
 def input(input):
     # x = re.search("{(.*)", option)
-    x = tm.parseInput(input)
+    x = tm.parseInput(input,tape)
     return x
 
 
