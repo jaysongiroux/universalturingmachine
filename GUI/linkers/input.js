8b624e349e294e8ed80fb9c4c69ca686c907b067
jaysongiroux <46802437+jaysongiroux@users.noreply.github.com>
2019-10-29 09:44:31 -0400

    added the directions page and button began working on the turing machine needs to support brackets and arrows does not communicate with the front end yet but working on that implemented some javascript parsing

--- GUI/linkers/input.js
+++ GUI/linkers/input.js
@@ -1,64 +1,130 @@
 let {PythonShell} = require('python-shell');
-var path = require("path");
+let path = require("path");
+const fs = require('fs');
 
-function checktape(tape){
-  // return true for now.
-  return true
+function tabs(evt, cityName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-// this is to check syntax for input
-function checkSyntax(input){
-  //  checks the input to determine if the input is valid
-  // if not input
 
-  // if it fits the regex return true
-  let re = new RegExp('{((((q([0-9]|[0-9]))|(R|L|Δ| |a|b|c|)),)*)(q([0-9]|[0-9])|(R|L|Δ| |a|b|c))}');
-  if (re.test(input)) {
-    return true
+function closeModal(){
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+function OpenModal(){
+  let modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+
+// tripper modals depending on turing machine
+function turingMachineStatus(status, finalTape = []){
+  let title = document.getElementById("modal-title");
+  let info = document.getElementById("modal-info");
+  if (status === true){
+    title.innerHTML = "Turing Machine Accepted";
+    info.innerHTML = finalTape;
+    OpenModal()
   }
 
-  else if (input === ""){
-    console.log('input is empty');
-    alert('Input Empty. please enter a valid input');
-    return false
-  }
-
-  else{
-    alert('Input invalid. please enter a valid input');
-    return false
+  else if(status === false){
+    title.innerHTML = "Turing Machine Was Unfavorable.";
+    info.innerHTML = "Turing Machine Was Unfavorable.";
+    OpenModal()
   }
 }
 
+function displayRadioValue() {
+  var ele = document.getElementsByName('example');
+  let exampleNum = 0;
+  for(i = 0; i < ele.length; i++) {
+    if(ele[i].checked)
+      exampleNum = ele[i].value;
+  }
+  return exampleNum
+}
+
+function parseFinalTape(str){
+  res = str.toString();
+  return res
+}
+
+// main function called from submit button
 function input(){
   let input = document.getElementById("INPUT").value;
   let tape = document.getElementById("TAPE").value;
+  let finaltape = document.getElementById("FINALTAPE");
 
-  let syntax = checkSyntax(input);
-  if (syntax === false){
-    return null
-  }
+  exampleNum = displayRadioValue();
 
-  let tapeSyntax = checktape(tape);
-  if (tapeSyntax === false){
-
-  }
+  // CREATE TAPE FILE
+  fs.writeFile('engine/tape.txt', tape, (err) => {
+    // In case of a error throw err.
+    if (err) throw err;
+  });
 
 
   let options = {
     scriptPath : path.join(__dirname, '/../engine/'),
-    args : [input,tape]
+    args : [input,exampleNum]
   };
 
   let pyshell = new PythonShell('interface.py', options);
 
   let output = document.getElementById('OUTPUT');
-  var array = [];
+  let encoding = document.getElementById("dynamicTable");
+  output.innerHTML = "";
+  encoding.innerText = "";
+  let l = [];
+  let counter = 1;
+  let temp = [];
 
   pyshell.on('message', function(message) {
-    array.push("\n[INPUT] - " + input);
-    array.push("\n[INPUT TAPE] - " + tape);
-    // evaluated = eval(message);
-    array.push(message);
-    output.innerHTML = array;
+    let tempMessage = "\n"+counter +": "+message;
+    output.innerHTML += tempMessage;
+    l.push(message);
+    counter ++;
+    if (tempMessage.includes("ACCEPTED")===true){
+      turingMachineStatus(true,l[counter-4]);
+      finaltape.innerHTML = parseFinalTape(l[counter-4]);
+//        show the check for accepted
+      document.getElementById("check").style.visibility= "visible";
+      document.getElementById("ex").style.visibility = "hidden";
+      document.getElementById("modal-header").style.backgroundColor ="#049700";
+      return true
+    }
+    // if the turing machine could not finish
+    else if(tempMessage.includes("STOPPED")===true){
+      finaltape.innerHTML = "Turing Machine Was Unfavorable.";
+      turingMachineStatus(false);
+//        display the X for rejected
+      document.getElementById("ex").style.visibility= "visible";
+      document.getElementById("check").style.visibility = "hidden";
+      // change modal to red for failed
+      document.getElementById("modal-header").style.backgroundColor ="#ff0000";
+    }
+    if (message.includes("Encoded State:")===true){
+      temp.push("\n"+message);
+      encoding.innerHTML += "\n <br>"+ message
+
+    }
   });
+  console.log(temp)
 }
--- engine/interface.py
+++ engine/interface.py
@@ -2,17 +2,23 @@ import requests
 import sys
 import turingMachine as tm
 import re
-
+import JSONConvert as con
+import exampleEngine as ex
 option = sys.argv[1]
-tape = sys.argv[2]
+example = sys.argv[2]
 
 # this is the script the javascript executes importing another python file for turing machines
-def input(input):
-    # x = re.search("{(.*)", option)
-    x,t,l = tm.parseInput(input,tape)
-    return l
+def input(input,exampleNum):
+    if exampleNum != 0:
+        ex.process(exampleNum)
+    else:
+        x = con.removebraces(input)
+        name, currentState, read, goto, write, move = con.seperater(x)
+        currentState = con.duplicate_states(currentState)
+        obj = con.jsonObj(name, currentState, read, goto, write, move)
+        con.creatJSON(obj)
+    tape = tm.main()
+    return tape
 
-
-print(input(option))
+input(option,example)
 sys.stdout.flush()
-
