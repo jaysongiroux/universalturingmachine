9ff919f2c00a92352b535cb05ef9ca2748aae2fc
jaysongiroux <jaysongiroux@gmail.com>
2019-11-08 17:32:12 -0500

    state on the GUI works and logging is better, still tracing some performance bugs

---
+++ engine/exampleEngine.py
@@ -0,0 +1,24 @@
+# this will be used if the user chooses an example in the GUI drop down
+import json
+import os
+import re
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def process(exampleNum):
+    tempString = ROOT_DIR+"/examples/"+str(exampleNum)+".json"
+
+    # if no example is choosen
+    if exampleNum == 0:
+        return null
+
+    # copy data from example json files to main data.json file
+    with open(tempString,"r") as json_file:
+        data = json.load(json_file)
+
+    with open(ROOT_DIR+"/data.json","w+")as json_file:
+        json.dump(data,json_file,indent=2)
+
+def test():
+    process(1)
+
+test()
