e7ac213c6970b6a98fb95ece5583442680461ff2
jaysongiroux <46802437+jaysongiroux@users.noreply.github.com>
2019-11-12 12:05:40 -0500

    added encoding, bug fixes, UI fixes and formatting

---
+++ engine/alphabet.py
@@ -0,0 +1,33 @@
+def alphabet(a):
+    if a == "a": return "1"
+    if a == "b": return "11"
+    if a == "c": return "111"
+    if a == "d": return "1111"
+    if a == "e": return "11111"
+    if a == "f": return "111111"
+    if a == "g": return "1111111"
+    if a == "h": return "11111111"
+    if a == "i": return "111111111"
+    if a == "j": return "1111111111"
+    if a == "k": return "11111111111"
+    if a == "l" or a=="L": return "111111111111"
+    if a == "m": return "1111111111111"
+    if a == "n": return "11111111111111"
+    if a == "o": return "111111111111111"
+    if a == "p": return "1111111111111111"
+    if a == "q": return "11111111111111111"
+    if a == "r" or a =="R": return "111111111111111111"
+    if a == "s": return "1111111111111111111"
+    if a == "t": return "11111111111111111111"
+    if a == "u": return "111111111111111111111"
+    if a == "v": return "1111111111111111111111"
+    if a == "w": return "11111111111111111111111"
+    if a == "x": return "111111111111111111111111"
+    if a == "y": return "1111111111111111111111111"
+    if a == "z": return "11111111111111111111111111"
+    # space
+    if a == "^" or a == " ": return "111111111111111111111111111"
+    if a == "-": return "1111111111111111111111111111"
+
+
+
---
+++ engine/encoding.py
@@ -0,0 +1,75 @@
+import json
+import turingMachine as tm
+import alphabet as alpha
+import re
+import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+jsonFile = ROOT_DIR+"/data.json"
+# read json file into obj file
+def readJSON():
+    with open(jsonFile) as json_file:
+        data = json.load(json_file)
+    return data
+
+def lengthJson(trans):
+    return len(trans)
+
+def getKeys(trans):
+    a = []
+    b = trans.keys()
+    for i in b:
+        a.append(i)
+    return a
+
+def numberbuilder(a):
+    b = [1] * int(a)
+    c = ''
+    for i in range(len(b)):
+        c +=str(b[i])
+    return c
+
+def encoding(trans):
+    final = []
+    states = getKeys(trans)
+    currentState, toRead, togo, write, move = tm.whatToRead(states,trans)
+#     get length of json file
+    for i in range(len(states)):
+        # q
+        en = []
+        en.append("1") # pushed for "q"
+        en.append("0") # trailing 0
+        temp = states[i]
+        temp = re.sub('q','',temp)
+        en.append(numberbuilder(temp[0])) #number following q
+        en.append("0") #trailing 0
+
+        # handels the dash and after the dash
+        if "-" in states[i]:
+            en.append(alpha.alphabet("-"))
+            en.append("0") # trailing 0
+            temp3 = re.search("-.*$",temp).group(0)
+            temp3 = re.sub('-','',temp3)
+            en.append(numberbuilder(temp3))
+
+        en.append('0')
+        en.append(alpha.alphabet(toRead[i]))
+        en.append('0')
+        en.append(alpha.alphabet('q'))
+        temp = re.sub('q','',togo[i])
+        en.append(numberbuilder(temp))
+        en.append('0')
+        en.append(alpha.alphabet(write[i].lower()))
+        en.append('0')
+        en.append(alpha.alphabet(move[i]))
+        temp = ''.join(en)
+        final.append({
+            states[i]:temp
+        })
+    return final
+
+
+
+def main():
+    test = readJSON()
+    return encoding(test)
